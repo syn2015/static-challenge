f004667e74fcc9a6831bdbd5a1b4b5c6289d3c72
syn2015 <syn201509@163.com>
2020-05-27 23:14:23 +0800

    首页页面开发，组件开发，配置viewport

---
+++ app_Finance/app/js/core/slider.vue
@@ -0,0 +1,58 @@
+<template >
+    <section :class="cname">
+        <swiper :options="options" :not-next-tick="options.notNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link :to="{ name: item.href}">
+                    <img :src="item.src" alt="">
+                </router-link>
+            </swiper-slide>
+            <!-- swiper spa的引用 -->
+            <div v-if="options.pagination" slot="pagination" class="swiper-pagination"/>
+        </swiper>
+    </section>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper"
+
+export default {
+    components: {
+        swiper,
+        swiperSlide,
+    },
+    props: {
+        options: {
+            type: Object,
+            default() {
+                return {
+                    autoplay: true,
+                    loop: true,
+                    pagination: {
+                        el: ".swiper-pagination",
+                    },
+                    notNextTick: false,
+                }
+            },
+        },
+        items: {
+            type: Array,
+            default() {
+                return []
+            },
+        },
+        cname: {
+            type: String,
+            default: "",
+        },
+    },
+    data() {
+        return {
+            panelClass: "panel",
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+@import '~swiper/dist/css/swiper';
+</style>
