ab1765f5f8de4bee3f09c6094209353895c0e947
syn2015 <syn201509@163.com>
2020-05-28 16:55:16 +0800

    新手特权组件，理财精选组件

--- app_Finance/app/js/home/index.vue
+++ app_Finance/app/js/home/index.vue
@@ -2,16 +2,25 @@
     <div >
         <heador/>
         <hslider/>
+        <novice/>
+        <borrow/>
+        <money/>
     </div>
 </template>
 
 <script>
 import Heador from "../public/header.vue"
 import Hslider from "../home/hslider.vue"
+import Novice from "./novice.vue"
+import Money from "./money.vue"
+import Borrow from "./borrow.vue"
 export default {
     components: {
         Heador,
         Hslider,
+        Novice,
+        Money,
+        Borrow,
     },
 }
 </script>
--- app_Finance/app/js/router/index.js
+++ app_Finance/app/js/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Home from "../home/index.vue"
+// import Money from "../Money/index.vue"
+// import Ious from "../ious/index.vue"
+// import Raise from "../raise/index.vue"
+// import Download from "../special/index.vue"
 import "../../css/reset.scss"
 
 
@@ -13,5 +17,26 @@ export default new Router({
             name: "Home",
             component: Home,
         },
+        // {
+        //     path: "/money",
+        //     name: "money",
+        //     component: Money,
+        // },
+        // {
+        //     path: "/ious",
+        //     name: "ious",
+        //     component: Ious,
+        // },
+        // {
+        //     path: "/raise",
+        //     name: "raise",
+        //     component: Raise,
+        // },
+        // {
+        //     path: "/download",
+        //     name: "download",
+        //     component: Download,
+        // },
+
     ],
 })
