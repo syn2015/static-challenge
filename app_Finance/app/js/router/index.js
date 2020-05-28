01840e318937b6b18a68036763f1d9f90da1ff73
syn2015 <syn201509@163.com>
2020-05-28 22:45:24 +0800

    理财页面开发

--- app_Finance/app/js/router/index.js
+++ app_Finance/app/js/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Home from "../home/index.vue"
-// import Money from "../Money/index.vue"
+import Money from "../money/index.vue"
 // import Ious from "../ious/index.vue"
 // import Raise from "../raise/index.vue"
 // import Download from "../special/index.vue"
@@ -17,11 +17,11 @@ export default new Router({
             name: "Home",
             component: Home,
         },
-        // {
-        //     path: "/money",
-        //     name: "money",
-        //     component: Money,
-        // },
+        {
+            path: "/money",
+            name: "money",
+            component: Money,
+        },
         // {
         //     path: "/ious",
         //     name: "ious",
