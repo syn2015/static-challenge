7880d67a725d6b4efdc7815b808c00378784aaa3
syn2015 <syn201509@163.com>
2020-05-29 14:03:39 +0800

    众筹页面的滚动条组件开发

---
+++ app_Finance/app/js/raise/index.vue
@@ -0,0 +1,41 @@
+
+
+<template lang="html">
+    <div >
+        <Heador/>
+        <Rslider/>
+        <Footer cname="mfooter"/>
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header.vue"
+import Footer from "../public/footer.vue"
+import Navbar from "../public/navbar.vue"
+import Rslider from "./rslider.vue"
+
+// import Adb from "./adb.vue"
+// import Finance from "./finance.vue"
+// import Adc from "./adc.vue"
+
+export default {
+    components: {
+        Heador,
+        Footer,
+        Navbar,
+        Rslider,
+    },
+}
+</script>
+
+<style lang="scss" >
+.mfooter{
+    margin-top: 0!important;
+    ul{
+        li:first-child{
+            display: none;
+        }
+    }
+}
+</style>
--- app_Finance/app/js/router/index.js
+++ app_Finance/app/js/router/index.js
@@ -3,7 +3,7 @@ import Router from "vue-router"
 import Home from "../home/index.vue"
 import Money from "../money/index.vue"
 import Ious from "../ious/index.vue"
-// import Raise from "../raise/index.vue"
+import Raise from "../raise/index.vue"
 // import Download from "../special/index.vue"
 import "../../css/reset.scss"
 
@@ -27,11 +27,11 @@ export default new Router({
             name: "ious",
             component: Ious,
         },
-        // {
-        //     path: "/raise",
-        //     name: "raise",
-        //     component: Raise,
-        // },
+        {
+            path: "/raise",
+            name: "raise",
+            component: Raise,
+        },
         // {
         //     path: "/download",
         //     name: "download",
