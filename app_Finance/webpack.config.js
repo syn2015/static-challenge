bc1c0451cd488e9f1ac0926ddaa95549b36d9aff
syn2015 <syn201509@163.com>
2020-05-29 20:45:44 +0800

    打包编译

--- app_Finance/webpack.config.js
+++ app_Finance/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 module.exports = env => {
   if (!env) {
     env = {}
@@ -20,7 +21,10 @@ module.exports = env => {
           NODE_ENV: '"production"'
         }
       }),
-      new ExtractTextPlugin("style.css", {ignoreOrder: true})
+      new ExtractTextPlugin("style.css", {ignoreOrder: true}),
+      new UglifyJsPlugin({
+        sourceMap:true
+      })
     )
   }
   return {
@@ -28,6 +32,7 @@ module.exports = env => {
       './app/js/viewport.js',
      './app/js/main.js'
   ],
+  devtool:'source-map',
     devServer: {
       contentBase: './dist',
       hot: true,
@@ -51,8 +56,8 @@ module.exports = env => {
             },
             extractCSS: true,
             loaders: env.production?{
-              css: ExtractTextPlugin.extract({use: 'css-loader!px2rem-loader?remUnit=40&remPrecision=8', fallback: 'vue-style-loader'}),
-              scss: ExtractTextPlugin.extract({use: 'css-loader!px2rem-loader?remUnit=40&remPrecision=8!sass-loader', fallback: 'vue-style-loader'})
+              css: ExtractTextPlugin.extract({use: 'css-loader?minimize!px2rem-loader?remUnit=40&remPrecision=8', fallback: 'vue-style-loader'}),
+              scss: ExtractTextPlugin.extract({use: 'css-loader?minimize!px2rem-loader?remUnit=40&remPrecision=8!sass-loader', fallback: 'vue-style-loader'})
             }:{
               css: 'vue-style-loader!css-loader!px2rem-loader?remUnit=40&remPrecision=8',
               scss: 'vue-style-loader!css-loader!px2rem-loader?remUnit=40&remPrecision=8!sass-loader'
