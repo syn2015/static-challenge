57a7d8a6305af624fd7a49b48b35977efd6286b5
syn2015 <syn201509@163.com>
2020-06-07 13:39:20 +0800

    vue router setting

---
+++ app_movie/routers/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import MovieRouter from './Movie'
+import MineRouter from './Mine'
+import CinemaRouter from './Cinema'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    MovieRouter,
+    MineRouter,
+    CinemaRouter
+   
+  ]
+})
