81d2bfcc72f46ab7dd2b3879a58caaeccc8f5667
syn2015 <syn201509@163.com>
2020-06-10 21:44:51 +0800

    部署

--- app_movie/vue.config.js
+++ app_movie/vue.config.js
@@ -1,5 +1,9 @@
-module.exports= {
+module.exports = {
+    // publicPath:'/AppMovie',
     devServer: {
+        disableHostCheck: true,
+        host: '192.168.43.2',
+
         proxy: {
             '/api': {
                 target: 'http://39.97.33.178',
